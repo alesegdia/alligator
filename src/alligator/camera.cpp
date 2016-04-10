a7e34591c39a36d6fab3905826aaf7122f2537d6
Alejandro Seguí <alesegdia@gmail.com>
2016-04-10 14:44:49 +0200

    adding viewport to camera; moving defs to .cpp

---
+++ src/alligator/camera.cpp
@@ -0,0 +1,40 @@
+
+#include "camera.h"
+
+void Camera::bind()
+{
+	al_identity_transform(&m_transform);
+	al_build_transform(&m_transform, m_position.x(), m_position.y(), m_scale.x(), m_scale.y(), m_rotation);
+	al_use_transform(&m_transform);
+}
+
+void Camera::position(Vec2f new_position)
+{
+	m_position = new_position;
+}
+
+float Camera::x()
+{
+	return m_position.x();
+}
+
+float Camera::y()
+{
+	return m_position.y();
+}
+
+void Camera::position(float x, float y)
+{
+	m_position.set(x, y);
+}
+
+void Camera::move(float x, float y)
+{
+	m_position.set(m_position.x() + x, m_position.y() + y);
+}
+
+void Camera::scale(float x, float y)
+{
+	m_scale.set(x, y);
+}
+
--- src/alligator/camera.h
+++ src/alligator/camera.h
@@ -7,27 +7,25 @@ class Camera
 {
 public:
 
-	void bind()
+	Camera( Vec2f viewport )
+		: m_viewport(viewport)
 	{
-		al_identity_transform(&m_transform);
-		al_build_transform(&m_transform, m_position.x(), m_position.y(), m_scale.x(), m_scale.y(), m_rotation);
-		al_use_transform(&m_transform);
+
 	}
 
-	void position( float x, float y )
-	{
-		m_position.set(x, y);
-	}
+	void bind();
 
-	void move( float x, float y )
-	{
-		m_position.set(m_position.x() + x, m_position.y() + y);
-	}
+	void position( Vec2f new_position );
 
-	void scale( float x, float y )
-	{
-		m_scale.set(x, y);
-	}
+	float x();
+
+	float y();
+
+	void position( float x, float y );
+
+	void move( float x, float y );
+
+	void scale( float x, float y );
 
 private:
 	ALLEGRO_TRANSFORM m_transform;
@@ -36,5 +34,9 @@ private:
 	Vec2f m_scale = Vec2f(0,0);
 	Vec2f m_position = Vec2f(0,0);
 	float m_rotation = 0;
+
+	Vec2f m_viewport;
+
+};
 
 };
