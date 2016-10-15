cd781daad82425e6aa22c00bdd80e0c0a75f528a
Alejandro Seguí <alesegdia@gmail.com>
2016-10-15 23:52:47 +0200

    distance function for vec2

--- src/alligator/util/math.h
+++ src/alligator/util/math.h
@@ -80,6 +80,26 @@ std::ostream &operator<<( std::ostream& os, const Vec2<T>& v )
 typedef Vec2<int> Vec2i;
 typedef Vec2<float> Vec2f;
 
+namespace allig::math {
+
+template <typename T>
+T distance( const Vec2<T>& a, const Vec2<T>& b )
+{
+	double dx = double(a.x()) - double(b.x());
+	double dy = double(a.y()) - double(b.y());
+	return sqrt(dx * dx + dy * dy);
+}
+
+template <typename T>
+T distancef( const Vec2<T>& a, const Vec2<T>& b )
+{
+	float dx = float(a.x()) - float(b.x());
+	float dy = float(a.y()) - float(b.y());
+	return sqrtf(dx * dx + dy * dy);
+}
+
+}
+
 template <typename T>
 class Rect
 {
