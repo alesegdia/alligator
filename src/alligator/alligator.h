37f1c39e3d0052bf313350c9f4d1410e451a2763
Alejandro Seguí <alesegdia@gmail.com>
2017-03-29 22:48:05 +0200

    adding common include file

---
+++ src/alligator/alligator.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "audio/audiosample.h"
+#include "audio/audiostream.h"
+
+#include "camera/camera.h"
+
+#include "game/game.h"
+#include "game/iscreen.h"
+
+#include "graphics/animation.h"
+#include "graphics/spritesheet.h"
+
+#include "input/iinputprocessor.h"
+#include "input/input.h"
+
+#include "util/math.h"
+#include "util/matrix.h"
+#include "util/rng.h"
